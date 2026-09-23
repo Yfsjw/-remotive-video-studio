4692173ccc6b480f48d91e1e025e048fa79f4725
Yfsjw <1championsleague2@gmail.com>
2026-09-23 12:05:52 +0100

    Set composition to 30 seconds

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,17 +1,6 @@
 import {Composition} from 'remotion';
 import {Main} from './Main';
 
-export const Root = () => {
-  return (
-    <>
-      <Composition
-        id="Main"
-        component={Main}
-        durationInFrames={150}
-        fps={30}
-        width={1080}
-        height={1920}
-      />
-    </>
-  );
-};
+export const Root = () => (
+  <Composition id="Main" component={Main} durationInFrames={900} fps={30} width={1080} height={1920} />
+);
