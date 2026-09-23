067161154319545a995a5ab34992b8d55a44d11b
Yfsjw <1championsleague2@gmail.com>
2026-09-23 18:51:29 +0100

    Switch prototype to five-second visual architecture

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,6 +1,6 @@
 import {Composition} from 'remotion';
-import {Main} from './Main';
+import {Scene01} from './Scene01';
 
 export const Root = () => (
-  <Composition id="Main" component={Main} durationInFrames={900} fps={30} width={1080} height={1920} />
+  <Composition id="Scene01" component={Scene01} durationInFrames={150} fps={30} width={1080} height={1920} />
 );
