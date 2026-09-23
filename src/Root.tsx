27968c04a9dbbff778a4db294c1c5964d4e93a54
Yfsjw <1championsleague2@gmail.com>
2026-09-23 21:22:32 +0100

    Set prototype to 24-second composition

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,5 +2,5 @@ import {Composition} from 'remotion';
 import {Main} from './Main';
 
 export const Root = () => (
-  <Composition id="Main" component={Main} durationInFrames={900} fps={30} width={1080} height={1920} />
+  <Composition id="Main" component={Main} durationInFrames={720} fps={30} width={1080} height={1920} />
 );
