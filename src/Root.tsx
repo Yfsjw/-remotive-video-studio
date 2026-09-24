0ef1c0813708ed5b63746d287a2fbf897aa023a9
Yfsjw <1championsleague2@gmail.com>
2026-09-24 14:41:06 +0100

    fix: register semantic shot composition

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,6 +1,11 @@
+import React from 'react';
 import {Composition} from 'remotion';
 import {Main} from './Main';
+import {ShotBased} from './ShotBased';
 
 export const Root = () => (
-  <Composition id="Main" component={Main} durationInFrames={900} fps={30} width={1080} height={1920} />
+  <>
+    <Composition id="Main" component={Main} durationInFrames={900} fps={30} width={1080} height={1920} />
+    <Composition id="ShotBased" component={ShotBased} durationInFrames={900} fps={30} width={1080} height={1920} />
+  </>
 );
